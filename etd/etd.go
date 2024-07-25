7b452c7c75ef00d0ffe6357d79fc5b20993a9962
Faisal Reza <7farema@gmail.com>
2024-07-25 13:30:31 +0700

    change the format of the time execution field in ETD to be minute-based

--- etd/etd.go
+++ etd/etd.go
@@ -226,7 +226,7 @@ func (lru LRU) PrintToFile(file *os.File, timeStart time.Time) (err error) {
 	file.WriteString(fmt.Sprintf("read count: %d\n", lru.readCount))
 	file.WriteString(fmt.Sprintf("hit ratio: %8.4f\n", (float32(lru.hit)/float32(lru.hit+lru.miss))*100))
 	file.WriteString(fmt.Sprintf("runtime: %8.4f\n", float32(lru.readCount)*lru.readCost+float32(lru.writeCount)*(lru.writeCost+lru.eraseCost)))
-	file.WriteString(fmt.Sprintf("time execution: %8.4f\n\n", time.Since(timeStart).Seconds()))
+	file.WriteString(fmt.Sprintf("time execution: %8.4f minute(s)\n\n", time.Since(timeStart).Minutes()))
 
 	return nil
 }
